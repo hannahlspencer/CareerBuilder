55786d5fbbcb5ac97eb86f07386984df9f3c5830
Matt Wildman <matthewwildman@gmail.com>
2013-05-09 17:19:57 +0100

    redesign and decision section fleshed out

--- careerBuilder.js
+++ careerBuilder.js
@@ -4,6 +4,8 @@ var noSelectMessage = "You have not selected any";
 $('.section-content').hide();
 $('.sub-content').hide();
 $('.suggestion').hide();
+$('.advice p').hide();
+$('#other-barriers').hide();
 $('#skills-summary').append("<li>" + noSelectMessage + " skills.</li>");
 $('#values-summary').append("<li>" + noSelectMessage + " values.</li>");
 
@@ -12,8 +14,9 @@ $('.toggler').click(function() { //toggle section/subsection visibility
   if($(this).is('h2')) {
 	var headID = $(this).attr('id');
 	$('h2.toggler').each(function() {
-		if ($(this).attr('id') != headID)
+		if ($(this).attr('id') != headID) {
 			$(this).siblings().hide('slow');
+		}
 	});
   }
 });
@@ -27,27 +30,37 @@ $('input:radio').change(function() { //expand advice according to radio button s
   });
 });
 
-$('input:checkbox').change(function() { //add selected checkbox items to summary lists
-	$('#skills-summary').html("");
-	$('#values-summary').html("");
-	var skillsCount = 0;
-	var valsCount = 0;
-	$("input:checkbox").each(function() {
-		if ($(this).is(':checked')) {
+$('input:checkbox').change(function() { 
+	if ($(this).attr('name') == 'skill' || $(this).attr('name') == 'value') { //add selected checkbox items to summary lists
+		$('#skills-summary').html("");
+		$('#values-summary').html("");
+		$('#skills-values-suggestion').show('slow');
+		var skillsCount = 0;
+		var valsCount = 0;
+		$('input:checkbox').each(function() {
 			var checkName = $(this).attr('name');
 			var checkVal = $(this).attr('value');
-			if (checkName == 'skill') {
-				$('#skills-summary').append("<li>" + checkVal + "</li>");
-				skillsCount++;
+			if ($(this).is(':checked')) {
+				if (checkName == 'skill') {
+					$('#skills-summary').append("<li>" + checkVal + "</li>");
+					skillsCount++;
+				}
+				else if (checkName == 'value') {
+					$('#values-summary').append("<li>" + checkVal + "</li>");
+					valsCount++;
+				}
 			}
-			else if (checkName == 'value') {
-				$('#values-summary').append("<li>" + checkVal + "</li>");
-				valsCount++;
-			}
+		});
+		if (skillsCount == 0)
+			$('#skills-summary').append("<li>" + noSelectMessage + " skills.</li>");
+		if (valsCount == 0)
+			$('#values-summary').append("<li>" + noSelectMessage + " values.</li>");
+		if (skillsCount == 0 && valsCount == 0)
+			$('#skills-values-suggestion').hide('slow');
+	}
+	else if ($(this).attr('name') == 'barrier') {
+		if ($(this).attr('value') == 'Other factors') {
+			$('#other-barriers').toggle('slow');
 		}
-	});
-	if (skillsCount == 0)
-		$('#skills-summary').append("<li>" + noSelectMessage + " skills.</li>");
-	if (valsCount == 0)
-		$('#values-summary').append("<li>" + noSelectMessage + " values.</li>");
+	}
 });
