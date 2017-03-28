9bcf8c193c71917b56a4e366c7155dcb41ac63eb
Matt Wildman <matthewwildman@gmail.com>
2017-03-28 11:56:56 +0100

    Small tab/spaces edit

--- app/scripts/careerBuilder.js
+++ app/scripts/careerBuilder.js
@@ -1052,7 +1052,7 @@ function registerHandlers() {
  $('.popup').keyup(function(event) {
     event = event || window.event;
     if (event.keyCode === 27) {
-	  closePopup($(this).closest('.popup').attr('id'));
+      closePopup($(this).closest('.popup').attr('id'));
     }
     return false;
   });
